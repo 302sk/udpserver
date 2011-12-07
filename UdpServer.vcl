c5adc711b18e7fa55b8f3800971a6a7602bf29ed
shenkun <302sk@163.com>
2011-12-07 14:45:30 +0800

    new project

---
+++ UdpServer.vcl
@@ -0,0 +1,33 @@
+<html>
+<body>
+<pre>
+<h1>Build Log</h1>
+<h3>
+--------------------Configuration: UdpServer - Win32 (WCE x86) Debug--------------------
+</h3>
+<h3>Command Lines</h3>
+Creating temporary file "C:\DOCUME~1\shen.kun\LOCALS~1\Temp\RSP87.tmp" with contents
+[
+/nologo /W3 /Zi /Od /D "DEBUG" /D "_i386_" /D UNDER_CE=500 /D _WIN32_WCE=500 /D "WCE_PLATFORM_AdvUNO2173AMUI" /D "i_386_" /D "UNICODE" /D "_UNICODE" /D "_X86_" /D "x86" /Fp"X86Dbg/UdpServer.pch" /YX /Fo"X86Dbg/" /Fd"X86Dbg/" /Gs8192 /GF /c 
+"D:\sktest\UdpServer\server.cpp"
+]
+Creating command line "cl.exe @C:\DOCUME~1\shen.kun\LOCALS~1\Temp\RSP87.tmp" 
+Creating temporary file "C:\DOCUME~1\shen.kun\LOCALS~1\Temp\RSP88.tmp" with contents
+[
+commctrl.lib coredll.lib corelibc.lib  /nologo /base:"0x00010000" /stack:0x10000,0x1000 /entry:"mainWCRTStartup" /incremental:yes /pdb:"X86Dbg/UdpServer.pdb" /debug /nodefaultlib:"OLDNAMES.lib" /nodefaultlib:libc.lib /nodefaultlib:libcd.lib /nodefaultlib:libcmt.lib /nodefaultlib:libcmtd.lib /nodefaultlib:msvcrt.lib /nodefaultlib:msvcrtd.lib /out:"X86Dbg/UdpServer.exe" /subsystem:windowsce,5.00 /MACHINE:IX86  
+.\X86Dbg\server.obj
+]
+Creating command line "link.exe @C:\DOCUME~1\shen.kun\LOCALS~1\Temp\RSP88.tmp"
+<h3>Output Window</h3>
+Compiling...
+server.cpp
+Linking...
+
+
+
+
+<h3>Results</h3>
+UdpServer.exe - 0 error(s), 0 warning(s)
+</pre>
+</body>
+</html>
---
+++ server.cpp
@@ -0,0 +1,47 @@
+//#include "stdafx.h"                     //预设环境
+#include <stdio.h>
+#include "winsock2.h"                   //包含网络处理函数头文件
+#pragma comment(lib,"Ws2.lib")       //包含静态链接库
+int WINAPI 
+_tmain(int argc, char* argv[])        //主函数开始
+{
+    WSADATA server;                     //定义服务结构体
+    WSAStartup(MAKEWORD(2,2),&server);  //初始化网络环境
+    SOCKET serversock=socket(AF_INET,SOCK_DGRAM,IPPROTO_IP);    //创建socket套接口
+    SOCKADDR_IN local;                  //定义地址结构
+    memset(&local,0,sizeof(local));     //地址结构清零
+    local.sin_addr.s_addr=INADDR_ANY;   //定义地址结构的IP
+    local.sin_family=AF_INET;           //定义地址结构类型
+    local.sin_port=htons(8000);         //定义地址结构的端口
+    SOCKADDR_IN client;                 //定义服务结构体
+    int len=sizeof(client);             //定义客户结构体
+    int ret=1;                          //定义出错函数
+    bind(serversock,(SOCKADDR*)&local,sizeof(local));       //绑定套接口
+    char buf[512]={0};                  //定义接收地址空间
+	char *ptrip=NULL;
+	char name[32]="";
+
+	 PHOSTENT hostinfo;
+	 if(gethostname (name, sizeof(name)) == 0)
+	 { 
+		  //如果成功 将本地主机名存放入name缓冲区
+		  if((hostinfo = gethostbyname(name)) != NULL) 
+		  { 
+		   //获取主机IP
+		   ptrip = inet_ntoa (*(struct in_addr *)*hostinfo->h_addr_list);
+		  }
+	 }
+
+	 printf("Host name:%s Host IP:%s\r\n",name, ptrip);
+	 
+    while(ret!=0)                       //循环接收
+    {
+        ret =recvfrom(serversock,buf,512,0,(SOCKADDR*)&client,&len);    //接收socket端口信息
+        buf[ret] = '\0';                //设置接收信息结束符
+        printf("%s\n",buf);             //输出显示信息
+    }
+    closesocket(serversock);            //关闭socket
+    WSACleanup();                       //清理网络环境
+    return 0;                           //返回0
+}
+ 
